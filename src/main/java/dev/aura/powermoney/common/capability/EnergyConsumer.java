3a4ab96038013acf59da3198323335d94845dc1f
BrainStone <yannick@tedworld.de>
2018-10-28 01:20:02 +0200

    Implemented EnergyConsumer

---
+++ src/main/java/dev/aura/powermoney/common/capability/EnergyConsumer.java
@@ -0,0 +1,77 @@
+package dev.aura.powermoney.common.capability;
+
+import com.google.common.collect.ImmutableMap;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import net.minecraftforge.energy.IEnergyStorage;
+
+@Value
+@RequiredArgsConstructor
+public class EnergyConsumer implements IEnergyStorage {
+  private static final Map<UUID, BigInteger> consumedEnergy = new HashMap<>();
+
+  private final UUID owner;
+
+  public static ImmutableMap<UUID, BigInteger> getAndResetConsumedEnergy() {
+    ImmutableMap<UUID, BigInteger> output = ImmutableMap.copyOf(consumedEnergy);
+
+    consumedEnergy.clear();
+
+    return output;
+  }
+
+  public EnergyConsumer() {
+    this(null);
+  }
+
+  @Override
+  public int receiveEnergy(int maxReceive, boolean simulate) {
+    if (!canReceive()) return 0;
+
+    if (!simulate) {
+      addEnergy(maxReceive);
+    }
+
+    return maxReceive;
+  }
+
+  @Override
+  public int extractEnergy(int maxExtract, boolean simulate) {
+    return 0;
+  }
+
+  @Override
+  public int getEnergyStored() {
+    return 0;
+  }
+
+  @Override
+  public int getMaxEnergyStored() {
+    return 0;
+  }
+
+  @Override
+  public boolean canExtract() {
+    return false;
+  }
+
+  @Override
+  public boolean canReceive() {
+    return owner != null;
+  }
+
+  private void addEnergy(long energy) {
+    final BigInteger currentValue = consumedEnergy.get(owner);
+    final BigInteger input = BigInteger.valueOf(energy);
+    BigInteger result;
+
+    if (currentValue == null) result = input;
+    else result = currentValue.add(input);
+
+    consumedEnergy.put(owner, result);
+  }
+}
--- src/main/java/dev/aura/powermoney/common/tileentity/TileEntityPowerReceiver.java
+++ src/main/java/dev/aura/powermoney/common/tileentity/TileEntityPowerReceiver.java
@@ -1,24 +1,23 @@
 package dev.aura.powermoney.common.tileentity;
 
+import dev.aura.powermoney.common.capability.EnergyConsumer;
 import java.util.UUID;
 import lombok.Getter;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ITickable;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.energy.CapabilityEnergy;
-import net.minecraftforge.energy.IEnergyStorage;
 
-public class TileEntityPowerReceiver extends TileEntity implements ITickable {
+public class TileEntityPowerReceiver extends TileEntity {
   @Getter private String customName;
 
   @Getter private UUID owner = null;
 
-  @Getter private IEnergyStorage energyStorage;
+  @Getter private EnergyConsumer energyConsumer = new EnergyConsumer();
 
   @Override
   public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
@@ -29,7 +28,7 @@ public class TileEntityPowerReceiver extends TileEntity implements ITickable {
   @SuppressWarnings("unchecked")
   @Override
   public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-    if (capability == CapabilityEnergy.ENERGY) return (T) energyStorage;
+    if (capability == CapabilityEnergy.ENERGY) return (T) energyConsumer;
 
     return super.getCapability(capability, facing);
   }
@@ -41,18 +40,19 @@ public class TileEntityPowerReceiver extends TileEntity implements ITickable {
   }
 
   public boolean hasCustomName() {
-    return this.customName != null && !this.customName.isEmpty();
+    return customName != null && !customName.isEmpty();
   }
 
   @Override
   public ITextComponent getDisplayName() {
     return this.hasCustomName()
-        ? new TextComponentString(this.customName)
+        ? new TextComponentString(customName)
         : new TextComponentTranslation("container.power_receiver");
   }
 
   public void setOwner(UUID newOwner) {
     owner = newOwner;
+    energyConsumer = new EnergyConsumer(owner);
 
     markDirty();
   }
@@ -75,10 +75,5 @@ public class TileEntityPowerReceiver extends TileEntity implements ITickable {
     else compound.removeTag("CustomName");
 
     return compound;
-  }
-
-  @Override
-  public void update() {
-    // TODO Auto-generated method stub
   }
 }
