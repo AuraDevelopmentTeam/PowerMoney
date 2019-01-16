b34cc59579d64c4f9f4ebbbaa446a6071d18cd18
BrainStone <yannick@tedworld.de>
2019-01-16 23:45:46 +0100

    Fix tests broken in 0d07b57

--- src/test/java/dev/aura/powermoney/common/payment/MoneyCalculatorTest.java
+++ src/test/java/dev/aura/powermoney/common/payment/MoneyCalculatorTest.java
@@ -108,43 +108,43 @@ public class MoneyCalculatorTest {
   public void randomValuesTest() {
     // 10 randomly generated pregenerated test cases
     assertEquals(
-        new BigDecimal("196.9014"),
+        new BigDecimal("142.0569"),
         new MoneyCalculator(6.098551413317902, 6.003164098841512)
             .covertEnergyToMoney(225352094716593244L));
     assertEquals(
-        new BigDecimal("189.7096"),
+        new BigDecimal("137.2312"),
         new MoneyCalculator(5.370389658603699, 5.204105648736891)
             .covertEnergyToMoney(387845730840177832L));
     assertEquals(
-        new BigDecimal("102.5033"),
+        new BigDecimal("74.0825"),
         new MoneyCalculator(2.040847212625775, 3.181678983927516)
             .covertEnergyToMoney(554236067197352565L));
     assertEquals(
-        new BigDecimal("238.0428"),
+        new BigDecimal("172.1154"),
         new MoneyCalculator(6.92887996519506, 5.44114450735833)
             .covertEnergyToMoney(345996023074805805L));
     assertEquals(
-        new BigDecimal("289.3434"),
+        new BigDecimal("208.5805"),
         new MoneyCalculator(3.5746568552972957, 2.040929244997452)
             .covertEnergyToMoney(586906925311361380L));
     assertEquals(
-        new BigDecimal("181.8706"),
+        new BigDecimal("132.3578"),
         new MoneyCalculator(6.474844362789973, 8.229980450187567)
             .covertEnergyToMoney(626686978358831832L));
     assertEquals(
-        new BigDecimal("231.6856"),
+        new BigDecimal("167.9663"),
         new MoneyCalculator(8.600623418074473, 8.807200665001467)
             .covertEnergyToMoney(321677944775318121L));
     assertEquals(
-        new BigDecimal("101.3922"),
+        new BigDecimal("73.4779"),
         new MoneyCalculator(3.414168605141459, 7.1806157740392305)
             .covertEnergyToMoney(371264184139064574L));
     assertEquals(
-        new BigDecimal("610.3009"),
+        new BigDecimal("440.9813"),
         new MoneyCalculator(9.257009365461581, 2.413807498485189)
             .covertEnergyToMoney(705202471905101987L));
     assertEquals(
-        new BigDecimal("12.4521"),
+        new BigDecimal("9.0693"),
         new MoneyCalculator(0.4037369324331397, 6.84643425079581)
             .covertEnergyToMoney(854980083071654808L));
   }
