c41868877913d98d134dc5981302b5ad222c10d2
Whitebrim <deni7off@yandex.ru>
2019-01-29 01:47:53 +0300

    Added MoneyCalculatorLog body

--- src/main/java/dev/aura/powermoney/common/payment/MoneyCalculatorLog.java
+++ src/main/java/dev/aura/powermoney/common/payment/MoneyCalculatorLog.java
@@ -10,51 +10,11 @@ import lombok.Value;
 
 @Value
 public class MoneyCalculatorLog extends MoneyCalculator {
-  private static final MathContext CALCULATION_PRECISION = MathContext.DECIMAL128;
-  private static final int RESULT_DIGITS = 4;
-  private static final RoundingMode RESULT_ROUNDING_MODE = RoundingMode.HALF_EVEN;
-
-  private final boolean useLog;
   
-  @Getter(AccessLevel.NONE)
-  private final BigDecimal baseMultiplierBD;
-  /**
-   * = 1 / log2(logBase)<br>
-   * <br>
-   * This is a constant used to calculate the log of the money.<br>
-   * Since we only have a log2 for BigInteger, the formula is <code>log_b(x) = log_2(x) / log_2(b)
-   * </code>. So we can precalculate <code>log_2(b)</code>. And since multiplication is faster than
-   * division we calculate the inverse too, so we just need to multiply it later.
-   */
-  @Getter(AccessLevel.NONE)
-  private final BigDecimal shiftBD;
-  @Getter(AccessLevel.NONE)
-  private final BigDecimal CalcHelper;
-  
-  public MoneyCalculator(double baseMultiplier, double calcBase) { //for compatibility with old versions
-    this(true, calcBase, baseMultiplier, 0.0);
-  }
-  
-  public MoneyCalculator(boolean useLog, double calcBase, double baseMultiplier, double shift) {
-    this.useLog = useLog;
-    shiftBD = BigDecimal.valueOf(shift);
-    baseMultiplierBD = BigDecimal.valueOf(baseMultiplier);
-    
-    if(useLog){
-      CalcHelper =
-        BigDecimal.ONE.divide(
-            BigDecimal.valueOf(Math.log(calcBase) / Math.log(2.0)), CALCULATION_PRECISION);
-    }
-    else{
-      CalcHelper = BigDecimal.valueOf(calcBase);
-    }
-  }
-
   public BigDecimal covertEnergyToMoney(long energy) {
     if (energy < 0) throw new IllegalArgumentException("energy must not be negative");
     else if (energy == 0) return BigDecimal.ZERO;
 
-  if(useLog){
     // baseMultiplier * ((CalcHelper * log2(money)) + 1)
     // which is also
     // baseMultiplier * (log_logBase(money) + 1)
@@ -66,19 +26,6 @@ public class MoneyCalculatorLog extends MoneyCalculator {
                 .add(BigDecimal.ONE, CALCULATION_PRECISION),
             CALCULATION_PRECISION),
         CALCULATION_PRECISION));
-    }
-    else{
-      return roundResult(
-        shiftBD.add(
-          baseMultiplierBD.multiply(
-            root(energy, CalcHelper.doubleValue()),
-          CALCULATION_PRECISION),
-        CALCULATION_PRECISION));
-    }
-  }
-
-  private static BigDecimal root(long val, double base) {
-    return BigDecimal.valueOf(Math.pow(val, 1.0 / base));
   }
             
   private static double log2(long val) {
