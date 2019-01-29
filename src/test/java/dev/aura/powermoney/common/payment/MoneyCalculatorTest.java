60d686a830f0080a0d6bca80afbcd4adbc5fafc6
Whitebrim <deni7off@yandex.ru>
2019-01-30 00:15:39 +0300

    Added root calculations

--- src/test/java/dev/aura/powermoney/common/payment/MoneyCalculatorTest.java
+++ src/test/java/dev/aura/powermoney/common/payment/MoneyCalculatorTest.java
@@ -42,7 +42,7 @@ public class MoneyCalculatorTest {
     for (MoneyCalculatorLog calculator : defaultMoneyCalculatorsLog) {
       assertEquals(
           "Case: " + calculator.toString(),
-          calculator.getBaseMultiplierBD(),
+          new BigDecimal(calculator.getBaseMultiplier()),
           calculator.covertEnergyToMoney(1L));
     }
   }
@@ -52,7 +52,7 @@ public class MoneyCalculatorTest {
     for (MoneyCalculatorLog calculator : defaultMoneyCalculatorsLog) {
       assertEquals(
           "Case: " + calculator.toString(),
-          new BigDecimal(calculator.getBaseMultiplierBD() * 2.0),
+          new BigDecimal(calculator.getBaseMultiplier() * 2.0),
           calculator.covertEnergyToMoney(2L));
     }
   }
@@ -75,7 +75,7 @@ public class MoneyCalculatorTest {
     for (MoneyCalculatorLog calculator : calculatorsLog) {
       assertEquals(
           "Case: " + calculator.toString(),
-          new BigDecimal(calculator.getBaseMultiplierBD() * 2.0),
+          new BigDecimal(calculator.getBaseMultiplier() * 2.0),
           calculator.covertEnergyToMoney(10L));
     }
   }
