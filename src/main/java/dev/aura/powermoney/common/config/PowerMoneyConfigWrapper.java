60ae6dc5b069b5874b83c43428f1ec374848a8a9
Whitebrim <deni7off@yandex.ru>
2019-01-29 21:26:05 +0300

    Added root calculations

--- src/main/java/dev/aura/powermoney/common/config/PowerMoneyConfigWrapper.java
+++ src/main/java/dev/aura/powermoney/common/config/PowerMoneyConfigWrapper.java
@@ -1,6 +1,8 @@
 package dev.aura.powermoney.common.config;
 
 import dev.aura.powermoney.common.payment.MoneyCalculator;
+import dev.aura.powermoney.common.payment.MoneyCalculatorLog;
+import dev.aura.powermoney.common.payment.MoneyCalculatorRoot;
 import java.util.List;
 import lombok.Getter;
 import net.minecraftforge.common.config.ConfigElement;
@@ -17,10 +19,15 @@ public class PowerMoneyConfigWrapper {
 
   private static Configuration configStorage;
 
-  @Getter private static boolean useLog;
-  @Getter private static double calcBase;
-  @Getter private static double baseMultiplier;
-  @Getter private static double shift;
+  @Getter private static double logBase;
+  @Getter private static double logBaseMultiplier;
+  @Getter private static double logShift;
+  
+  @Getter private static double rootBase;
+  @Getter private static double rootBaseMultiplier;
+  @Getter private static double rootShift;
+  
+  @Getter private static int calcType;
   @Getter private static MoneyCalculator moneyCalculator;
 
   @Getter private static String currency;
@@ -44,43 +51,88 @@ public class PowerMoneyConfigWrapper {
   }
 
   private static void loadCalculationSettings() {
-    useLog =
-        getBoolean(
+    calcType =
+        getInt(
             CAT_CALCULATION,
-            "UseLog",
-            true,
+            "CalcType",
+            0,
+			0,
+			1,
             "Choose the type of calculation."
-                + "true - logarithm [Shift + BaseMultiplier * (log_CalcBase(EnergyPerSecond) + 1)]"
-                + "false - root [Shift + BaseMultiplier * root_CalcBase(EnergyPerSecond)");
-    calcBase =
+                + "0 - logarithm [Shift + BaseMultiplier * (log_CalcBase(EnergyPerSecond) + 1)]"
+                + "1 - root [Shift + BaseMultiplier * root_CalcBase(EnergyPerSecond)");
+
+    logBase =
         getDouble(
             CAT_CALCULATION,
-            "CalcBase",
+            "LogBase",
             2,
             Math.nextUp(1.0),
             1E6,
-            "The logarithmic or root base in the calculation.\n"
-                + "Depends on which type of calculation is chosen."
+			"LOGARITHMIC CALCULATION\n"
+				+ "#######################\n"
+				+ "The logarithmic base in the calculation.\n"
                 + "The higher the value the less money the players get.");
-    baseMultiplier =
+    logBaseMultiplier =
         getDouble(
             CAT_CALCULATION,
-            "BaseMultiplier",
+            "LogBaseMultiplier",
             0.10,
             1E-6,
             1E6,
-            "The base multiplier in the calculation.\n"
+            "The base multiplier in the log calculation.\n"
                 + "Essentially how much 1 unit of energy per second is worth.");
-    shift =
+    logShift =
         getDouble(
             CAT_CALCULATION,
-            "Shift",
+            "LogShift",
             0,
             -1E10,
             1E10,
-            "Еhe value that will be added each time to the final result.");
+            "The value that will be added each time to the final log calculation result."
+				+ "Helps to adjust the energy price.");
 
-    moneyCalculator = new MoneyCalculator(useLog, calcBase, baseMultiplier, shift);
+    rootBase =
+        getDouble(
+            CAT_CALCULATION,
+            "RootBase",
+            2,
+            Math.nextUp(1.0),
+            1E6,
+			"ROOT CALCULATION\n"
+				+ "################\n"
+				+ "The root base in the calculation.\n"
+                + "The higher the value the less money the players get.");
+    rootBaseMultiplier =
+        getDouble(
+            CAT_CALCULATION,
+            "RootBaseMultiplier",
+            0.10,
+            1E-6,
+            1E6,
+            "The base multiplier in the root calculation.\n"
+                + "Essentially how much 1 unit of energy per second is worth.");
+    rootShift =
+        getDouble(
+            CAT_CALCULATION,
+            "RootShift",
+            0,
+            -1E10,
+            1E10,
+            "The value that will be added each time to the final root calculation result."
+				+ "Helps to adjust the energy price.");
+
+	switch (calcType) {
+           case  (0):
+               moneyCalculator = new MoneyCalculatorLog(logBase, logBaseMultiplier, logShift);
+               break;
+           case (1):
+               moneyCalculator = new MoneyCalculatorRoot(rootBase, rootBaseMultiplier, rootShift);
+               break;
+           default:
+               throw new IllegalArgumentException("Unknown calculation type.");;
+               break;
+    }
 
     addCustomCategoryComment(
         CAT_CALCULATION,
@@ -89,10 +141,10 @@ public class PowerMoneyConfigWrapper {
             + "You can choose one of two formulas for calculating the energy price.\n"
             + "\n"
             + "Logarithmic formula:\n"
-            + "    MoneyPerSecond = Shift + BaseMultiplier * (log_LogBase(EnergyPerSecond) + 1)\n"
+            + "    MoneyPerSecond = LogShift + LogBaseMultiplier * (log_LogBase(EnergyPerSecond) + 1)\n"
             + "\n"
             + "Root formula:\n"
-            + "    MoneyPerSecond = Shift + BaseMultiplier * root_RootBase(EnergyPerSecond)");
+            + "    MoneyPerSecond = RootShift + RootBaseMultiplier * root_RootBase(EnergyPerSecond)");
   }
 
   private static void loadPaymentSettings() {
