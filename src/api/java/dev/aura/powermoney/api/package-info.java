07477f961763dda5a198d1ac187b9654f2c2d2cc
BrainStone <yannick@tedworld.de>
2019-03-08 19:52:58 +0100

    Added API package

---
+++ src/api/java/dev/aura/powermoney/api/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package and it's subpackages contain the APi for the PowerMoney mod.
+ *
+ * @author BrainStone
+ */
+@net.minecraftforge.fml.common.API(owner = "@id@", provides = "@id@|api", apiVersion = "@version@")
+package dev.aura.powermoney.api;
